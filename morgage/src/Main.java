504854c988de72dc1838761f4f8a9afc812a36f1
Yonas Sh <yonakifle@yahoo.com>
2023-06-15 00:32:35 -0700

    morgageCalculator Refactored

---
+++ morgage/src/Main.java
@@ -0,0 +1,22 @@
+import com.yonas.Console;
+import com.yonas.MorgageCalculator;
+import com.yonas.MorgageReport;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
+
+
+        var calculator = new MorgageCalculator(principal,annualInterest,years);
+
+        var report = new MorgageReport(calculator);
+        report.printMortgage();
+        report.printPaymentSchedule();
+    }
+
+}
